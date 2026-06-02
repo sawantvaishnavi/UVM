eed8df35e5f44ed0f081522a16e835bb0f6ffc12
Vaishnavi Sawant <vdsawant.vs@gmail.com>
2026-06-02 10:24:24 +0530

    Create 07.SHALLOW COPY.sv

---
+++ 01.Field Macros/07.SHALLOW COPY.sv
@@ -0,0 +1,54 @@
+//understand shallow copy
+
+`include "uvm_macros.svh";
+import uvm_pkg::*;
+
+class first extends uvm_object;
+  
+  rand bit [3:0] addr;
+  
+  function new(string path = "first");
+    super.new(path);
+  endfunction
+  
+  `uvm_object_utils_begin(first)
+  `uvm_field_int(addr, UVM_DEFAULT);
+  `uvm_object_utils_end
+  
+endclass
+
+class second extends uvm_object;
+  
+  first f;
+  
+  function new(string path = "second");
+    super.new(path);
+    f = new("first");
+  endfunction
+  
+  `uvm_object_utils_begin(second)
+  `uvm_field_object(f, UVM_DEFAULT);
+  `uvm_object_utils_end
+endclass
+
+
+module tb;
+  
+  second s1, s2;
+  
+  initial begin
+    s1 = new("s1");
+    s2 = new("s2");
+    
+    s1.randomize();
+    s1.print();
+    
+    s2 = s1; //shallow copy
+    s2.print();
+    
+    s2.f.addr = 10;   //as we change s2, this change reflects in s1 also
+    s1.print();
+    s2.print();
+    
+  end
+endmodule
